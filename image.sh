bb6b31cc2708508f112c9bd9fbd0c74e59a6ff7a
Matt C <mdc028@bucknell.edu>
2022-07-21 09:11:27 -0400

    no sig check

--- image.sh
+++ image.sh
@@ -31,7 +31,7 @@ fi
 
 mkdir ${UNPACK_TGT}
 
-pacstrap -C strap.conf ${UNPACK_TGT} linux-vm filesystem busybox bash linux-firmware pacman rustysd zsh
+pacstrap -M -G -C strap.conf ${UNPACK_TGT} linux-vm filesystem busybox bash linux-firmware pacman rustysd zsh
 
 size=$(du -sh ${UNPACK_TGT} | awk '{ print $1 }')
 
